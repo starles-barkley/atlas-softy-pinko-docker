108fb49a7193a3cc6736613b8d368ba5a2a6974a
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-04 22:17:11 +0000

    update api file

--- task3/back-end/api.py
+++ task3/back-end/api.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/hello')
 def hello_world():
@@ -8,4 +10,3 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5252)
-
